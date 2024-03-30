685c5c0b492969119f2f8d8946b1994f9ef94fd6
nikitonya <nikitonya_s@mail.ru>
2024-03-30 21:21:00 +0400

    manifest fix + take the url of the current page

--- phish.js
+++ phish.js
@@ -1,18 +1,24 @@
 const grab = document.getElementById("grab");
 grab.addEventListener("click", async () => {
     console.log("Button clicked");
-    const url = "http://camphhsi.com/product/list_947.html";
-    try {
-        const response = await postData('http://127.0.0.1:5000/predict', {url});
-        console.log(response);
-        if (response.predicted_class === 0) {
-            alert("Это не фишинг");            
-        } else if (response.predicted_class === 1) {
-            alert("Это ФИШИНГ!")
+    chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+        const currentUrl = tabs[0].url;
+        console.log(currentUrl);
+        console.log(typeof(currentUrl))
+        try {
+            const response = await postData('http://127.0.0.1:5000/predict', { url: currentUrl });
+            console.log(response);
+            if (response.predicted_class === 0) {
+                alert("Это не фишинг");            
+            } else if (response.predicted_class === 1) {
+                alert("Это ФИШИНГ!")
+            } else if (response.error) {
+                alert("Ошибка")
+            }
+        } catch (error) {
+            console.error('Error:', error);
         }
-    } catch (error) {
-        console.error('Error:', error);
-    }
+    });
 });
 
 const postData = async (url = '', data = {}) => {
