9fe6ae753e12abffcf502f3fa676d367719813da
nikitonya <nikitonya_s@mail.ru>
2024-03-31 17:48:18 +0400

    clear code

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-async function postData(url = '', data = {}) {
+const postData = async (url = '', data = {}) => {
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -13,17 +13,9 @@ async function postData(url = '', data = {}) {
     }
 }
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
     if (changeInfo.url) {
         console.log(changeInfo.url);
-        postData('http://127.0.0.1:5000/predict', { url: changeInfo.url });
-        
+        await postData('http://127.0.0.1:5000/predict', { url: changeInfo.url });
     }
 });
-
-// chrome.tabs.onActivated.addListener(function (activeInfo) {
-//     chrome.tabs.get(activeInfo.tabId, function (tabInfo) {
-//         console.log(tabInfo.url);
-//         postData('http://127.0.0.1:5000/predict', { url: tabInfo.url });
-//     });
-// });
--- popup.js
+++ popup.js
@@ -2,17 +2,15 @@ const grab = document.getElementById("grab");
 
 grab.addEventListener("click", async () => {
     console.log("Button clicked");
-    chrome.tabs.query({active: true, currentWindow: true}, async function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
         const currentUrl = tabs[0].url;
-        console.log(currentUrl);
-        console.log(typeof(currentUrl))
         try {
             const response = await postData('http://127.0.0.1:5000/predict', { url: currentUrl });
             console.log(response);
             if (response.predicted_class === 0) {
-                alert("Это не фишинг");            
+                alert("Non-phishing url");
             } else if (response.predicted_class === 1) {
-                alert("Это ФИШИНГ!")
+                alert("Phishing url")
             } else if (response.error) {
                 alert("Ошибка")
             }
