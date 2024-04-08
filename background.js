33eb33ec5f481bd1ea5942fe80b00a5b9327826d
nikitonya <nikitonya_s@mail.ru>
2024-04-08 15:03:04 +0400

    url verification is working

--- background.js
+++ background.js
@@ -13,9 +13,55 @@ const postData = async (url = '', data = {}) => {
     }
 }
 
-chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-    if (changeInfo.url) {
-        console.log(changeInfo.url);
-        await postData('http://127.0.0.1:5000/predict', { url: changeInfo.url });
-    }
-});
+// // background url sending function when updating the url
+// chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo) {
+//     const url = changeInfo.url;
+//     if (url && !isUnwantedUrl(url)) {
+//         console.log(url);
+//         const response = await postData('http://127.0.0.1:5000/predict', { url })
+//         console.log(response)
+        
+//         chrome.tabs.sendMessage(tabId, {
+//             message: response
+//         });
+//     }   
+// });
+
+function isUnwantedUrl(url) {
+    return url.startsWith('chrome');
+}
+
+// development of sending the html code of the page
+
+// chrome.browserAction.onClicked.addListener(function () {
+//     chrome.tabs.query({}, function (tabs) {
+//         for (var i = 0; i < tabs.length; i++) {
+//             var id = tabs[i].id;
+//             var url = tabs[i].url;
+//             //method1(id);
+//             method2(id);
+//             //method3(url);
+//         }
+//     });
+// });
+
+// function method1(tabId) {
+//     chrome.tabs.executeScript(tabId, { "code": "document.documentElement.outerHTML;" }, function (result) {
+//         console.log(result);
+//     });
+// }
+
+// function method2(id) {
+//     chrome.tabs.sendMessage(id, {action: "getSource"}, function(response) {
+//         console.log(response.sourceCode);
+//     });
+// }
+
+// function method3(url) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.onload = function () {
+//         console.log(xhr.responseText);
+//     };
+//     xhr.open("GET", url);
+//     xhr.send();
+// }
--- popup.js
+++ popup.js
@@ -1,18 +1,23 @@
-const grab = document.getElementById("grab");
-
-grab.addEventListener("click", async () => {
-    console.log("Button clicked");
+const url = document.getElementById("URL");
+url.addEventListener("click", async () => {
+    console.log("Button URL clicked");
     chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
         const currentUrl = tabs[0].url;
+        if (currentUrl.startsWith('chrome')) {
+            alert("SAFE");
+            return;
+        }
         try {
-            const response = await postData('http://127.0.0.1:5000/predict', { url: currentUrl });
-            console.log(response);
+            const response = await postData('http://127.0.0.1:5000/predict', { url: currentUrl });    
+            console.log("url = ", currentUrl);
+            console.log("response = ", response);
             if (response.predicted_class === 0) {
-                alert("Non-phishing url");
+                alert(currentUrl + " - SAFE URL")
             } else if (response.predicted_class === 1) {
-                alert("Phishing url")
+                alert(currentUrl + " - PHISHING URL")
             } else if (response.error) {
-                alert("Ошибка")
+                alert("Ошибка");
+                console.log("ERROR" + response.error);
             }
         } catch (error) {
             console.error('Error:', error);
@@ -34,3 +39,40 @@ const postData = async (url = '', data = {}) => {
         throw new Error('Error fetching data:', error);
     }
 }
+
+// development of sending the html code of the page
+
+// const html = document.getElementById("HTML");
+// html.addEventListener("click", async () => {
+//     console.log("Button HTML clicked");
+//     chrome.tabs.query({}, function (tabs) {
+//         for (var i = 0; i < tabs.length; i++) {
+//             var id = tabs[i].id;
+//             var url = tabs[i].url;
+//             //method1(id);
+//             method2(id);
+//             //method3(url);
+//         }
+//     });
+// })
+
+// function method1(tabId) {
+//     chrome.tabs.executeScript(tabId, { "code": "document.documentElement.outerHTML;" }, function (result) {
+//         console.log(result);
+//     });
+// }
+
+// function method2(id) {
+//     chrome.tabs.sendMessage(id, {action: "getSource"}, function(response) {
+//         console.log(response.sourceCode);
+//     });
+// }
+
+// function method3(url) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.onload = function () {
+//         console.log(xhr.responseText);
+//     };
+//     xhr.open("GET", url);
+//     xhr.send();
+// }
